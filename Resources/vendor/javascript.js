e8658248d7bf243148b2d47c0b818434ee86f582
ShavinduDeveloper <shavindu.thushara.2001.developer@gmail.com>
2021-06-19 07:42:04 +0530

    initial commit

---
+++ Resources/vendor/javascript.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*====================================================
+ CV
+ ====================================================*/
+$(function () {
+    $(".title").addClass("animated fadeInDown");
+    $(".data").addClass("animated fadeInDown");
+    $(".ol").addClass("animated zoomIn");
+    $(".al").addClass("animated zoomIn");
+    $(".list1").addClass("animated fadeInRight");
+    $(".list2").addClass("animated fadeInRight");
+    $("#card1").addClass("animated bounceIn");
+    $("#card2").addClass("animated bounceIn");
+    $("#card3").addClass("animated bounceIn");
+    $("#card4").addClass("animated bounceIn");
+    $("#card5").addClass("animated bounceIn");
+    $("#card6").addClass("animated bounceIn");
+    $("#p1").addClass("animated bounceIn");
+    $("#p2").addClass("animated bounceIn");
+    $("#p3").addClass("animated bounceIn");
+    $("#p4").addClass("animated bounceIn");
+    $("#p5").addClass("animated bounceIn");
+    $("#p6").addClass("animated bounceIn");
+    $(".social-link-f li").addClass("animated bounceIn");
+});
